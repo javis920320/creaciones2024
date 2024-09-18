fdedb9c405e38990c78bb090cfff9845b8fca9ac
Javier Lopez <javis9203@gmail.com>
2024-09-18 09:54:38 -0500

    update  vista dasboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,26 +1,105 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+import Section from "@/Components/Section";
+import SecondaryButton from "@/Components/SecondaryButton";
+import Sidebar from "@/Layouts/Sidebar";
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({ auth, clientes, empleados, products }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Dashboard
+                </h2>
+            }
         >
             <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
+            <main className="flex">
+                <Sidebar />
+                <div className=" grid  grid-cols-3 gap-2 items-baseline">
+                <Section className=" flex justify-center items-center  ">
+                        <h1 className="text-gray-500 dark:text-gray-200 text-2x1">
+                            Tus Pedidos{" "}
+                        </h1>
+                        <p className="text-gray-700 dark:text-gray-200">
+                            Introduce nuestro dinámico panel de pedidos para una
+                            gestión fluida y un análisis perspicaz.
+                        </p>
+                        <SecondaryButton>
+                            <Link href="/crear-pedidos">Crear Nuevo pedio</Link>
+                        </SecondaryButton>
+                    </Section>
+                    <Section className="w-1/3 flex justify-center items-center ">
+                        <h1 className="text-gray-500 dark:text-gray-200 text-2x1 text-center">
+                            Asignar Pedidos
+                        </h1>
+                        <p className="text-gray-700 dark:text-gray-200 text-center">
+                            {clientes}
+                        </p>
+                        <SecondaryButton>
+                            {" "}
+                            <Link href={route("asignacion.index")}>
+                                Asignar{" "}
+                            </Link>
+                        </SecondaryButton>
+                    </Section>
+                    <Section className="w-1/5">
+                        <h1 className="text-gray-500 dark:text-gray-200 text-2x1 text-center">
+                            Corte y Confección
+                        </h1>
+                        <p className="text-gray-700 dark:text-gray-200 text-center">
+                            {empleados}
+                        </p>
+                        <SecondaryButton>
+                            {" "}
+                            <Link href={route("corteConfeccion.index")}>
+                                Ver Proceso
+                            </Link>
+                        </SecondaryButton>
+                    </Section>
+                    
+                   {/*  
+                    
+                    <Section className="w-1/3 flex justify-center items-center ">
+                        <h1 className="text-gray-500 dark:text-gray-200 text-2x1 text-center">
+                            Clientes registrados
+                        </h1>
+                        <p className="text-gray-700 dark:text-gray-200 text-center">
+                            {clientes}
+                        </p>
+                        <SecondaryButton>
+                            {" "}
+                            <Link href="/clients">Ver clientes</Link>
+                        </SecondaryButton>
+                    </Section>
+                    <Section className="w-1/3">
+                        <h1 className="text-gray-500 dark:text-gray-200 text-2x1 text-center">
+                            Empleados registrados
+                        </h1>
+                        <p className="text-gray-700 dark:text-gray-200 text-center">
+                            {empleados}
+                        </p>
+                        <SecondaryButton>
+                            {" "}
+                            <Link href="/employees">Ver Empleado</Link>
+                        </SecondaryButton>
+                    </Section>
+                    <Section className="w-1/3">
+                        <h1 className="text-gray-500 dark:text-gray-200 text-2x1 text-center">
+                            Productos
+                        </h1>
+                        <p className="text-gray-700 dark:text-gray-200 text-center">
+                            {products}
+                        </p>
+                        <SecondaryButton>
+                            {" "}
+                            <Link href="/products">Ver Productos</Link>
+                        </SecondaryButton>
+                    </Section>
+                     */}
                 </div>
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+            </main>
         </AuthenticatedLayout>
     );
 }
