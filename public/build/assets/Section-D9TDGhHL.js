cc68ca7cc3e7c299094970509b701a1fbeab9362
Javier Lopez <javis9203@gmail.com>
2024-10-16 16:17:01 -0500

    probando cambios

---
+++ public/build/assets/Section-D9TDGhHL.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CHCnxYMc.js";const e=({children:s,...t})=>o.jsx("section",{...t,className:" mt-4 p-4 sm:p-8  bg-white dark:bg-gray-800 shadow sm:rounded-lg",children:s});export{e as S};
